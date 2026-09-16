51811baa9fbcbbd015b76e222ed0aa9e87824e13
SimiImmaculate24 <24ds2000040@ds.study.iitm.ac.in>
2026-09-16 19:48:14 +0530

    Remove hardcoded configuration secrets

--- app/config.py
+++ app/config.py
@@ -1,8 +1,6 @@
-# app/config.py
 import os
 
-SECRET = os.getenv("QUIZ_SECRET", "Immaculate")
-ALLOWED_EMAIL = os.getenv("QUIZ_EMAIL", "24ds2000040@ds.study.iitm.ac.in")
+SECRET = os.getenv("QUIZ_SECRET")
 TIMEOUT_SECONDS = int(os.getenv("QUIZ_TIMEOUT", "170"))
 
 # Default to headless unless otherwise set
